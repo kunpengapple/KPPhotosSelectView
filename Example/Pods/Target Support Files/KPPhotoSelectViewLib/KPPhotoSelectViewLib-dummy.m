acc8c06798cbe7d86f1cd37593c38534ec6aec71
linian <linian@liniandeiMac.local>
2018-07-01 21:45:05 +0800

    组件初始化

---
+++ Example/Pods/Target Support Files/KPPhotoSelectViewLib/KPPhotoSelectViewLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KPPhotoSelectViewLib : NSObject
+@end
+@implementation PodsDummy_KPPhotoSelectViewLib
+@end
---
+++ Example/Pods/Target Support Files/KPPhotoSelectViewLib/KPPhotoSelectViewLib-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "KPImageSelectedView.h"
+#import "KPPhotoModel.h"
+#import "KPPhotoSubViewCell.h"
+#import "KPSelectViewCollectionView.h"
+
+FOUNDATION_EXPORT double KPPhotoSelectViewLibVersionNumber;
+FOUNDATION_EXPORT const unsigned char KPPhotoSelectViewLibVersionString[];
+
